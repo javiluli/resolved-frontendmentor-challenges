1b1db8a999fed7e310d19e3dd8b5f32a53c88fa5
javiluli <javiluli.dev@gmail.com>
2022-09-01 21:54:01 +0200

    se agrega el reto `product-preview-card-component`

--- src/api/data.js
+++ src/api/data.js
@@ -20,6 +20,25 @@ const challens = [
       "A perfect first challenge if you're new to HTML and CSS. The card layout doesn't shift, so it's ideal if you haven't learned about building responsive layouts yet.",
     codesandbox: 'https://codesandbox.io/s/qr-code-component-2k5jqt',
   },
+  {
+    id: 'product-preview-card-component',
+    title: 'Product preview card component',
+    img: 'https://res.cloudinary.com/dz209s6jk/image/upload/q_auto,w_700/Challenges/lftjtn81b7brs2icc898.jpg',
+    difficulty: 'newbie',
+    level: 1,
+    languages: [
+      {
+        title: 'HTML',
+        color: 'rgb(106, 190, 205)',
+      },
+      {
+        title: 'CSS',
+        color: 'rgb(62, 84, 163)',
+      },
+    ],
+    descripcion: 'This HTML & CSS-only challenge will be perfect for anyone starting to build responsive projects.',
+    codesandbox: 'https://codesandbox.io/s/product-preview-card-component-m8b6fu',
+  },
 ]
 
 export default challens
